49d7b641ce05e87e5a48e269c33626eb698838f5
Ravy Aryo <ravyaaryo@gmail.com>
2022-06-09 18:04:28 +0700

    -edit profile & settings

--- lib/screens/account/widget/appbar_account.dart
+++ lib/screens/account/widget/appbar_account.dart
@@ -1,3 +1,4 @@
+import 'package:cook_kuy/screens/account/settings_screen.dart';
 import 'package:cook_kuy/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -13,11 +14,11 @@ class AppbarAccount extends StatefulWidget {
 class _AppbarAccountState extends State<AppbarAccount> {
   @override
   Widget build(BuildContext context) {
-    return const SliverAppBar(
+    return SliverAppBar(
       pinned: true,
       backgroundColor: Colors.white,
       centerTitle: true,
-      title: Text(
+      title: const Text(
         "Account",
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -27,10 +28,20 @@ class _AppbarAccountState extends State<AppbarAccount> {
       ),
       actions: [
         Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Icon(
-            Icons.settings,
-            color: ijoSkripsi,
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SettingsScreen(),
+                ),
+              );
+            },
+            icon: const Icon(
+              Icons.settings,
+              color: ijoSkripsi,
+            ),
           ),
         )
       ],
