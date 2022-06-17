b430bd73217b5815ed70badfbf395c1b6bd8f557
Ravy Aryo <ravyaaryo@gmail.com>
2022-06-17 18:27:33 +0700

    -fix bug di atas screen item (ilangin safearea)

--- lib/screens/searchResult/search_result.dart
+++ lib/screens/searchResult/search_result.dart
@@ -17,94 +17,92 @@ class SearchResultScreen extends StatefulWidget {
 class _SearchResultScreenState extends State<SearchResultScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Search Result"),
-          centerTitle: true,
-          titleTextStyle: const TextStyle(
-              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
-          backgroundColor: Colors.white,
-          elevation: 1,
-          iconTheme: const IconThemeData(color: Colors.black),
-        ),
-        body: StreamBuilder(
-            stream:
-                // FirebaseFirestore.instance
-                //     .collection('recipe')
-                //     .where('ingredients', arrayContainsAny: widget.selectedIngredients)
-                //     .snapshots(),
-                widget.completeIngre.additionalIngre.isEmpty?
-                FirebaseFirestore.instance.collection('recipe').where('main_ingredient', isEqualTo: widget.completeIngre.mainIngre).snapshots() : 
-                FirebaseFirestore.instance.collection('recipe').where('main_ingredient', isEqualTo: widget.completeIngre.mainIngre).where('ingredients', arrayContainsAny: widget.completeIngre.additionalIngre).snapshots(),
-            builder: (context,
-                AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-              if (!snapshot.hasData) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-
-              return Column(
-                children: [
-                  // Padding(
-                  //   padding:
-                  //       const EdgeInsets.only(left: 15, right: 15, top: 10),
-                  //   child: SizedBox(
-                  //     height: 40,
-                  //     child: ListView(
-                  //       scrollDirection: Axis.horizontal,
-                  //       children: [
-                  //         FilterChip(
-                  //             selected: true,
-                  //             label: const Text("Sarapan"),
-                  //             onSelected: (_) {}),
-                  //         const SizedBox(
-                  //           width: 10,
-                  //         ),
-                  //         FilterChip(
-                  //             selected: true,
-                  //             label: const Text("Makan Siang"),
-                  //             onSelected: (_) {}),
-                  //         const SizedBox(
-                  //           width: 10,
-                  //         ),
-                  //         FilterChip(
-                  //             selected: true,
-                  //             label: const Text("Makan Malam"),
-                  //             onSelected: (_) {})
-                  //       ],
-                  //     ),
-                  //   ),
-                  // ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Expanded(
-                    child: GridView.builder(
-                      padding: const EdgeInsets.only(
-                          bottom: 15, left: 15, right: 15),
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              childAspectRatio: 0.80,
-                              crossAxisSpacing: 25,
-                              mainAxisSpacing: 25),
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) => InkWell(
-                        onTap: () {
-                          Navigator.of(context, rootNavigator: true)
-                              .pushNamed(AppRouter.recipeDetail, arguments: snapshot.data!.docs[index].data());
-                        },
-                        child: SearchResultCard(
-                            snap: snapshot.data!.docs[index].data()),
-                      ),
-                    ),
-                  )
-                ],
-              );
-            }),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Search Result"),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
+      body: StreamBuilder(
+          stream:
+              // FirebaseFirestore.instance
+              //     .collection('recipe')
+              //     .where('ingredients', arrayContainsAny: widget.selectedIngredients)
+              //     .snapshots(),
+              widget.completeIngre.additionalIngre.isEmpty?
+              FirebaseFirestore.instance.collection('recipe').where('main_ingredient', isEqualTo: widget.completeIngre.mainIngre).snapshots() : 
+              FirebaseFirestore.instance.collection('recipe').where('main_ingredient', isEqualTo: widget.completeIngre.mainIngre).where('ingredients', arrayContainsAny: widget.completeIngre.additionalIngre).snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Column(
+              children: [
+                // Padding(
+                //   padding:
+                //       const EdgeInsets.only(left: 15, right: 15, top: 10),
+                //   child: SizedBox(
+                //     height: 40,
+                //     child: ListView(
+                //       scrollDirection: Axis.horizontal,
+                //       children: [
+                //         FilterChip(
+                //             selected: true,
+                //             label: const Text("Sarapan"),
+                //             onSelected: (_) {}),
+                //         const SizedBox(
+                //           width: 10,
+                //         ),
+                //         FilterChip(
+                //             selected: true,
+                //             label: const Text("Makan Siang"),
+                //             onSelected: (_) {}),
+                //         const SizedBox(
+                //           width: 10,
+                //         ),
+                //         FilterChip(
+                //             selected: true,
+                //             label: const Text("Makan Malam"),
+                //             onSelected: (_) {})
+                //       ],
+                //     ),
+                //   ),
+                // ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: GridView.builder(
+                    padding: const EdgeInsets.only(
+                        bottom: 15, left: 15, right: 15),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.80,
+                            crossAxisSpacing: 25,
+                            mainAxisSpacing: 25),
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) => InkWell(
+                      onTap: () {
+                        Navigator.of(context, rootNavigator: true)
+                            .pushNamed(AppRouter.recipeDetail, arguments: snapshot.data!.docs[index].data());
+                      },
+                      child: SearchResultCard(
+                          snap: snapshot.data!.docs[index].data()),
+                    ),
+                  ),
+                )
+              ],
+            );
+          }),
     );
   }
 }
